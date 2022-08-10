de80fcb9f7054b4b24d3369664f308f6e4f8aa46
CaroCats09 <99272069+CaroCats09@users.noreply.github.com>
2022-08-10 23:37:07 +0200

    Update LikeItem.vue

--- frontend/groupo/src/components/LikeItem.vue
+++ frontend/groupo/src/components/LikeItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card d-flex bg-white p-3 border-0 like-dislike-item">
         <div class="like-btn-container py-1 d-flex align-items-center justify-content-between">
-            <button type="button" @click="handleLike" class="p-2 like-btn border-0 d-flex justify-content-center align-items-center" >
+          <button type="button" @click="handleLike" class="p-2 like-btn border-0 d-flex justify-content-center align-items-center" :disabled="checkUserLikeClassBtn"  >
                 <i v-if="!isLiked" class="far fa-sun like-icon me-2" ref="icon"></i>
                 <i v-if="isLiked" class="fas fa-sun like-icon me-2" ref="icon"></i>
                 <span v-if="!isLiked" class="like-text color-grey" ref="text">Cool</span>
@@ -10,7 +10,7 @@
             <span class="count-like p-2">{{ countLikes }} Cool</span>
         </div>
         <div class="dislike-btn-container py-1 d-flex align-items-center justify-content-between">
-            <button type="button" @click="handleDislike" class="p-2 dislike-btn border-0 d-flex justify-content-center align-items-center" >
+         <button type="button" @click="handleDislike" class="p-2 dislike-btn border-0 d-flex justify-content-center align-items-center"  :disabled="checkUserDislikeClassBtn" >
                
                 <span v-if="!isDisliked" class="dislike-text color-grey-pascool" ref="text">Pas cool</span>
                 <span v-if="isDisliked" class="dislike-text color-groupo-pascool" ref="text">Pas cool</span>
@@ -25,7 +25,7 @@
     export default {
         name: 'LikeItem',
         props: {
-            postId: Number,
+            postId: String,
             likes: Number,
             dislikes: Number,
             usersLiked: Array,
@@ -34,11 +34,32 @@
         data() {
             return {
                 
-                countLikes: this.$props.likes,
-                countDisLikes: this.$props.dislikes
+              countLikes: this.$props.likes,
+                countDisLikes: this.$props.dislikes,
+                listUsersLiked: this.$props.usersLiked,
+                listUsersDisliked: this.$props.usersDisliked,
             };
         },
         computed: {
+        
+            //vérifier si l'utilisateur exist dans le tableau de dislike  return disabled
+        checkUserLikeClassBtn() {
+            if (this.listUsersDisliked.includes(localStorage.getItem("userId")) == false) { 
+               
+            return false;
+        }
+            else
+                return true;
+            },
+            //vérifier si l'utilisateur exist dans le tableau de like  return disabled
+        checkUserDislikeClassBtn() {
+          
+             if (this.listUsersLiked.includes(localStorage.getItem("userId")) == false)
+                return false;
+            else
+                return true;
+            },
+
             isLiked() {
                 const likesUserIdList = this.$props.usersLiked.map(like => like.userId);
                 const userId = this.$store.state.userId;
@@ -57,7 +78,8 @@
                 // Logique du like
                 const postId = this.$props.postId;
                 const token = localStorage.getItem("token");
-                let like = (this.$props.usersLiked.includes(localStorage.getItem("userId"))) ? 1 : 0;
+                let like = (this.listUsersLiked.includes(localStorage.getItem("userId"))) ? 1 : 0;
+                
                 if (like == 1) like = 0;
                 if (like == 0) like = 1;
                 try {
@@ -79,11 +101,15 @@
                     likeIcon.classList.replace('color-grey','color-groupo');
                     likeText.classList.replace('color-grey','color-groupo');
                     this.countLikes++;
+                    this.listUsersLiked.push(localStorage.getItem("userId"));
+                    
                 } else {
                     likeIcon.classList.replace('fas', 'far');
                     likeIcon.classList.remove('anim');
                     likeText.classList.replace('color-groupo','color-grey');
                     this.countLikes--;
+                    let position = this.listUsersLiked.indexOf(localStorage.getItem("userId"));
+                    this.listUsersLiked.splice(position, 1);
                 }
                 
             },
@@ -94,7 +120,8 @@
                 const postId = this.$props.postId;
                 const token = localStorage.getItem("token");
 
-                let like = (this.$props.usersDisliked.includes(localStorage.getItem("userId"))) ? -1 : 0;
+
+                let like = (this.listUsersDisliked.includes(localStorage.getItem("userId"))) ? -1 : 0;
                 if (like == -1) like = 0;
                 if (like == 0) like = -1;
                 try {
@@ -112,9 +139,12 @@
                 if
                     (dislikeText.classList.replace('color-grey-pascool', 'color-groupo-pascool')){
                     this.countDisLikes++;
+                    this.listUsersDisliked.push(localStorage.getItem("userId"));
                 } else {
                     (dislikeText.classList.replace('color-groupo-pascool', 'color-grey-pascool'))
                     this.countDisLikes--;
+                    let position = this.listUsersDisliked.indexOf(localStorage.getItem("userId"));
+                    this.listUsersDisliked.splice(position, 1);
                 }
             }
         }
